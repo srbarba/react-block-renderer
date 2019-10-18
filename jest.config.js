e5f7e03ceb7f28ed285bae0140385e68bc08e7be
Juan Pedro <jpbs92@hotmail.com>
2019-10-18 17:54:31 +0200

    Added babel/webpack to the project

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  roots: ['<rootDir>'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  testRegex: '(/tests/.*|(\\.|/)(test|spec))\\.tsx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  coverageReporters: ["json", "text", "html"]
+}
