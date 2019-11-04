b53e59aca7e6092a5858f3c160983ed294559a35
Juan Pedro <jpbs92@hotmail.com>
2019-11-04 16:39:48 +0100

    Fix webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,20 @@
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 const path = require('path');
-const APP_PATH = path.resolve(__dirname, 'src');
 
 module.exports = {
-  entry: APP_PATH,
+  entry: path.resolve(__dirname, 'src/index.tsx'),
 
   output: {
     filename: 'index.js',
     path: path.resolve(__dirname, 'build'),
+    libraryTarget: 'umd'
   },
 
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.json']
   },
+
+  devtool: 'source-map',
 
   module: {
     rules: [{ test: /\.(ts|js)x?$/, loader: 'babel-loader', exclude: /node_modules/ }],
